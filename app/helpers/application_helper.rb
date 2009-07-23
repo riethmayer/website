b61a40586cee17dea9cbe7f491943f42d58eba01
Jan Riethmayer <Jan@Kamehameha.local>
2009-07-23 23:36:07 +0200

    adding google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,4 +40,20 @@ module ApplicationHelper
     { :id     => (@document_style || YUI_CONFIG['width']) , 
       :class  => (@sidebar_style  || YUI_CONFIG['sidebar'] )}
   end
+  
+  def google_analytics_first
+    <<-GOOGLE
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    GOOGLE
+  end
+  
+  def google_analytics_last
+    <<-GOOGLE
+      try {
+      var pageTracker = _gat._getTracker("UA-9914295-1");
+      pageTracker._trackPageview();
+      } catch(err) {}
+    GOOGLE
+  end
 end
