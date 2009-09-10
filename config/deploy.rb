d1d3dc99ce0209ce101cd5581e4ac7065932147b
Jan Riethmayer <Jan@Kamehameha.local>
2009-09-11 00:20:45 +0200

    removing some copyright stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,11 @@ role :app, application
 role :web, application
 role :db,  application, :primary => true
 
-set :user, "deploy"
-set :deploy_to, "/home/deploy/www/#{application}"
+set :user, "optimiere"
+set :deploy_to, "/var/rails/optimiere"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :port, '10022'
 
 set :scm, "git"
 set :repository,  "git://github.com/riethmayer/website.git"
@@ -25,12 +26,16 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/3rd_party_apps.yml #{release_path}/config/3rd_party_apps.yml"
     run "ln -nfs #{shared_path}/config/disqus.yml #{release_path}/config/disqus.yml"
     run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
-    
+  end
+  
+  desc "overwrite start, as we use passenger"
+  task :start do
+    # nothing
   end
   
   desc "Sync the public/asets directory."
   task :assets do
-    system "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
+    system "rsync -vr --port=10022 --exclude='.DS_Store' public/assets #{user}@#{application}:#{shared_path}/"
   end
   # cap deploy:assets
 end
